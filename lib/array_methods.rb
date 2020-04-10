7951261d26b2637bfb52f253a76ae3499ee1dabd
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-10 16:53:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,5 @@
 def using_include(array, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?("wow") || array.include?("Tardis")
+array.include?("wow") 
 end
 
 def using_sort(array)
